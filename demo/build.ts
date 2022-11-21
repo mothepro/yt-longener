e75e7e9ccd51c2b0c39bdc263a1220e02b643298
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-11-21 02:06:30 -0600

    sample with html

---
+++ demo/build.ts
@@ -0,0 +1,35 @@
+import { readFile } from 'fs'
+import { parse } from 'node-html-parser'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+// util
+function assert(expression: unknown, message = 'Assertion Error'): asserts expression {
+  if (!expression) throw new Error(message)
+}
+function assertNotNull<T>(expression: T, message?: string): NonNullable<T> {
+  assert(expression != null, message)
+  return expression
+}
+
+const readFileAsync = (path: string): Promise<string> =>
+  new Promise((resolve, reject) =>
+    readFile(path, 'utf-8', (err, data) => (err ? reject(err) : resolve(data)))
+  )
+
+// file paths
+const htmlPath = resolve(dirname(fileURLToPath(import.meta.url)), 'placeholder.html')
+const srcPath = resolve(dirname(fileURLToPath(import.meta.url)), '../index.js')
+
+// get strings
+const html = assertNotNull(await readFileAsync(htmlPath))
+const src = assertNotNull(await readFileAsync(srcPath))
+
+// parse html
+const root = parse(html)
+
+// replace the `href`
+const codeEl = assertNotNull(root.querySelector('a#code')) as unknown as HTMLAnchorElement
+codeEl.setAttribute('href', `javascript:(function(){${ encodeURI(src) }})();`)
+
+console.log(root.outerHTML)
