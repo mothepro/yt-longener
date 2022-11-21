42b14f40d26129ba28f5254cc8b5abed97fb9196
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-11-21 01:13:45 -0600

    initial script

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-// Remember to append ".js" for relative imports
+function assert(expression: unknown, message = 'Assertion Error'): asserts expression {
+  if (!expression) throw new Error(message)
+}
+function assertNotNull<T>(expression: T, message?: string): NonNullable<T> {
+  assert(expression != null, message)
+  return expression
+}
+
+try {
+  const { host, pathname } = location
+  assert(host.includes('youtube.com'), 'Must be on youtube to use this bookmarklet')
+  const [, id] = assertNotNull(
+    pathname.match(/\/[a-zA-Z0-9_]+\/([a-zA-Z0-9_]+)/),
+    'Can not find youtube video id'
+  )
+
+  location.href = `https://youtu.be/${id}`
+} catch (err) {
+  assert(err instanceof Error)
+  alert(err.message)
+}
