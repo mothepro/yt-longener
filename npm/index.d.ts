e3278496c1d2c2f480df580ae57919d13da402ac
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-11-21 02:12:39 -0600

    Updates

--- npm/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function assert(expression: unknown, message?: string): asserts expression;
-export declare function assertNotNull<T>(expression: T, message?: string): NonNullable<T>;
