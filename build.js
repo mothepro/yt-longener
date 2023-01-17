cfad31392b56c15fc0aa05ff288a8165ef3a52d1
Mo Prosper <mothepro@live.com>
2023-01-16 18:44:33 -0600

    Updates

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ import { readFile } from 'fs';
 import { parse } from 'node-html-parser';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
+import { minify } from 'terser';
 // util
 function assert(expression, message = 'Assertion Error') {
     if (!expression)
@@ -17,10 +18,12 @@ const htmlPath = resolve(dirname(fileURLToPath(import.meta.url)), 'placeholder.h
 const srcPath = resolve(dirname(fileURLToPath(import.meta.url)), '../index.js');
 // get strings
 const html = assertNotNull(await readFileAsync(htmlPath));
-const src = assertNotNull(await readFileAsync(srcPath));
+const code = assertNotNull(await readFileAsync(srcPath));
+const minifiedCode = assertNotNull((await minify(code, { toplevel: true, mangle: true })).code);
 // parse html
 const root = parse(html);
 // replace the `href`
 const codeEl = assertNotNull(root.querySelector('a#bookmarklet'));
-codeEl.setAttribute('href', `javascript:(function(){${encodeURI(src)}})();`);
+codeEl.setAttribute('href', `javascript:(function(){${encodeURI(minifiedCode)}})();`);
+codeEl.setAttribute('poo', minifiedCode);
 console.log(root.outerHTML);
